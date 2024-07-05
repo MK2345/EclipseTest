116984e8f41bff492a75c757cdf4d0125d687919
MK2345 <lapsus4@gmx.de>
2024-07-05 13:41:48 +0200

    next commit

--- src/Test/Testclass.java
+++ src/Test/Testclass.java
@@ -6,6 +6,7 @@ public class Testclass {
 		// TODO Auto-generated method stub
 		System.out.println("Hello I'm Eclipse. See you tomorrow or not?");
 		System.out.println("Bye bye and good bye");
+		System.out.println("Hello again.");
 
 	}
 
